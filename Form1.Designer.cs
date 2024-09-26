ee404c1402062627613abb9d60061f3b16fefd53
chstkhvst <hellotherelol@gmail.com>
2024-09-26 20:42:31 +0300

    eng to rus, autosize

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,9 +32,22 @@
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.button2 = new System.Windows.Forms.Button();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sign_date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.id_employee = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.id_client = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.id_tour = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
@@ -49,12 +62,6 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button4 = new System.Windows.Forms.Button();
             this.dataGridView4 = new System.Windows.Forms.DataGridView();
-            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.sign_date = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.price = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.id_employee = new System.Windows.Forms.DataGridViewComboBoxColumn();
-            this.id_client = new System.Windows.Forms.DataGridViewComboBoxColumn();
-            this.id_tour = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -105,12 +112,69 @@
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column7,
+            this.Column6});
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(664, 331);
             this.dataGridView1.TabIndex = 1;
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "id";
+            this.Column1.HeaderText = "id";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "fullname";
+            this.Column2.HeaderText = "ФИО";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "birthday";
+            this.Column3.HeaderText = "Дата рождения";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.DataPropertyName = "passport";
+            this.Column4.HeaderText = "Паспорт";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.DataPropertyName = "international_passport";
+            this.Column5.HeaderText = "Загранпаспорт";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            // 
+            // Column7
+            // 
+            this.Column7.DataPropertyName = "gender";
+            this.Column7.HeaderText = "Пол";
+            this.Column7.MinimumWidth = 6;
+            this.Column7.Name = "Column7";
+            // 
+            // Column6
+            // 
+            this.Column6.DataPropertyName = "discount";
+            this.Column6.HeaderText = "% цены со скидкой";
+            this.Column6.MinimumWidth = 6;
+            this.Column6.Name = "Column6";
             // 
             // tabPage2
             // 
@@ -152,6 +216,59 @@
             this.dataGridView2.RowTemplate.Height = 24;
             this.dataGridView2.Size = new System.Drawing.Size(608, 382);
             this.dataGridView2.TabIndex = 0;
+            // 
+            // id
+            // 
+            this.id.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.id.DataPropertyName = "id";
+            this.id.HeaderText = "id";
+            this.id.MinimumWidth = 6;
+            this.id.Name = "id";
+            // 
+            // sign_date
+            // 
+            this.sign_date.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.sign_date.DataPropertyName = "sign_date";
+            this.sign_date.HeaderText = "Дата подписания";
+            this.sign_date.MinimumWidth = 6;
+            this.sign_date.Name = "sign_date";
+            // 
+            // price
+            // 
+            this.price.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.price.DataPropertyName = "price";
+            this.price.HeaderText = "Цена";
+            this.price.MinimumWidth = 6;
+            this.price.Name = "price";
+            // 
+            // id_employee
+            // 
+            this.id_employee.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.id_employee.DataPropertyName = "id_employee";
+            this.id_employee.HeaderText = "Сотрудник";
+            this.id_employee.MinimumWidth = 6;
+            this.id_employee.Name = "id_employee";
+            this.id_employee.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.id_employee.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // id_client
+            // 
+            this.id_client.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.id_client.DataPropertyName = "id_client";
+            this.id_client.HeaderText = "Клиент";
+            this.id_client.MinimumWidth = 6;
+            this.id_client.Name = "id_client";
+            this.id_client.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.id_client.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // id_tour
+            // 
+            this.id_tour.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.id_tour.DataPropertyName = "id_tour";
+            this.id_tour.HeaderText = "Тур";
+            this.id_tour.MinimumWidth = 6;
+            this.id_tour.Name = "id_tour";
+            this.id_tour.Resizable = System.Windows.Forms.DataGridViewTriState.True;
             // 
             // tabPage3
             // 
@@ -259,11 +376,11 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(398, 33);
+            this.label4.Location = new System.Drawing.Point(370, 36);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(310, 20);
+            this.label4.Size = new System.Drawing.Size(438, 20);
             this.label4.TabIndex = 3;
-            this.label4.Text = "Сколько туров купили в страну?";
+            this.label4.Text = "Сколько туров купили в страну за все время?";
             // 
             // textBox1
             // 
@@ -293,53 +410,6 @@
             this.dataGridView4.RowTemplate.Height = 24;
             this.dataGridView4.Size = new System.Drawing.Size(303, 225);
             this.dataGridView4.TabIndex = 0;
-            // 
-            // id
-            // 
-            this.id.DataPropertyName = "id";
-            this.id.HeaderText = "id";
-            this.id.MinimumWidth = 6;
-            this.id.Name = "id";
-            // 
-            // sign_date
-            // 
-            this.sign_date.DataPropertyName = "sign_date";
-            this.sign_date.HeaderText = "sign_date";
-            this.sign_date.MinimumWidth = 6;
-            this.sign_date.Name = "sign_date";
-            // 
-            // price
-            // 
-            this.price.DataPropertyName = "price";
-            this.price.HeaderText = "price";
-            this.price.MinimumWidth = 6;
-            this.price.Name = "price";
-            // 
-            // id_employee
-            // 
-            this.id_employee.DataPropertyName = "id_employee";
-            this.id_employee.HeaderText = "employee";
-            this.id_employee.MinimumWidth = 6;
-            this.id_employee.Name = "id_employee";
-            this.id_employee.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.id_employee.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
-            // 
-            // id_client
-            // 
-            this.id_client.DataPropertyName = "id_client";
-            this.id_client.HeaderText = "client";
-            this.id_client.MinimumWidth = 6;
-            this.id_client.Name = "id_client";
-            this.id_client.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.id_client.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
-            // 
-            // id_tour
-            // 
-            this.id_tour.DataPropertyName = "id_tour";
-            this.id_tour.HeaderText = "id_tour";
-            this.id_tour.MinimumWidth = 6;
-            this.id_tour.Name = "id_tour";
-            this.id_tour.Resizable = System.Windows.Forms.DataGridViewTriState.True;
             // 
             // Form1
             // 
@@ -393,6 +463,13 @@
         private System.Windows.Forms.DataGridViewComboBoxColumn id_employee;
         private System.Windows.Forms.DataGridViewComboBoxColumn id_client;
         private System.Windows.Forms.DataGridViewTextBoxColumn id_tour;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
     }
 }
 
